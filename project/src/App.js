c89ada55a0667ffc9df75483e766013e926b63ab
LYJ <tometora44@gmail.com>
2025-10-13 17:58:25 +0900

    2

--- project/src/App.js
+++ project/src/App.js
@@ -1,30 +1,17 @@
-<<<<<<< HEAD
 import './App.css';
+import { Routes, Route,Outlet } from 'react-router-dom'
 import Main from './component/Main'
 import Detail from './component/Detail';
 import Myfooter from './component/Myfooter';
 import Navigation from './component/Navigation';
-=======
-import './App.css'
-import { Routes, Route,Outlet } from 'react-router-dom'
-import Main from './component/Main'
-import Myfooter from './component/Myfooter'
-import Navigation from './component/Navigation'
->>>>>>> 140e36d4758e7a2ab1ce03faa6bc1825d2665e31
 
 function Layout() {
   return (
     <>
-<<<<<<< HEAD
     <Navigation/>
     {/* <Main /> */}
     <Detail />
     <Myfooter />
-=======
-      <Navigation />
-      <Outlet /> 
-      <Myfooter />
->>>>>>> 140e36d4758e7a2ab1ce03faa6bc1825d2665e31
     </>
   )
 }
