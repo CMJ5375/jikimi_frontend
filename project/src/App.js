e02298b535cc3884fdf07b448f115dac312bec08
LYJ <tometora44@gmail.com>
2025-10-14 17:43:10 +0900

    ㅁㄴㅇㄹ

--- project/src/App.js
+++ project/src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
 import { Routes, Route} from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import Main from './component/Main';
+import Navigation from './component/Navigation'
+import Myfooter from './component/Myfooter'
 import HospitalDetail from './component/HospitalDetail';
 import PharmacyDetail from './component/PharmacyDetail';
-import './App.css'
 import MyPage from './component/MyPage'
 import LoginMain from './component/LoginMain'
 import Register from './component/Register'
-import Navigation from './component/Navigation'
-import { Outlet } from 'react-router-dom';
-import Myfooter from './component/Myfooter'
+
 
 function Layout() {
   //깃 연동을 위한 임시 주석
