5ff8d0e1de5f86ac6898382b6ecb904ce7b95452
CMJ5375 <siny0913@naver.com>
2025-10-14 17:39:31 +0900

    Update App.js

--- project/src/App.js
+++ project/src/App.js
@@ -3,8 +3,8 @@ import { Routes, Route} from 'react-router-dom';
 import Main from './component/Main';
 import HospitalDetail from './component/HospitalDetail';
 import PharmacyDetail from './component/PharmacyDetail';
-import Layout from './component/Layout';
-import './App.css'
+import Myfooter from './component/Myfooter'
+import Navigation from './component/Navigation'
 import MyPage from './component/MyPage'
 import LoginMain from './component/LoginMain'
 import Register from './component/Register'
@@ -19,8 +19,6 @@ function Layout() {
     </>
   )
 }
-import HospitalDetail from './component/HospitalDetail';
-import PharmacyDetail from './component/PharmacyDetail';
 
 export default function App() {
   return (
