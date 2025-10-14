959cd88e3248a5dcf5e5478998a6db889448762f
llimdo <ldy110322@gmail.com>
2025-10-14 17:56:59 +0900

    마지막 추합

--- project/src/App.js
+++ project/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { Routes, Route} from 'react-router-dom';
+import { Routes, Route ,Outlet } from 'react-router-dom';
 import Main from './component/Main';
 import HospitalDetail from './component/HospitalDetail';
 import PharmacyDetail from './component/PharmacyDetail';
@@ -8,6 +8,8 @@ import LoginMain from './component/LoginMain'
 import Register from './component/Register'
 import FindUser from './component/FindUser'
 import Noticeboard from './component/Noticeboard'
+import Navigation from './component/Navigation';
+import Myfooter from './component/Myfooter';
 function Layout() {
  
   return (
@@ -27,17 +29,10 @@ export default function App() {
         <Route path='mypage' element={<MyPage />} />
         <Route path="login" element={<LoginMain />} />
         <Route path="register" element={<Register/>} />
-<<<<<<< HEAD
-        <Route path="hospitaldetails" element={<HospitalDetail />} />
-        <Route path="pharmacydetails" element={<PharmacyDetail />} />
-        <Route path='noticeboards' element={<Noticeboard/>}/>
-        <Route path='hospitalDetails' element={<HospitalDetail/>}/>
-=======
         <Route path='finduser' element={<FindUser/>}/>
         <Route path="pharmacydetail" element={<PharmacyDetail />} />
         <Route path="hospitaldetail" element={<HospitalDetail />} />
         <Route path='noticeboards' element={<Noticeboard/>}/>
->>>>>>> eede293674d3ab37b6484ad53894b998d1454def
       </Route>
     </Routes>
   )
