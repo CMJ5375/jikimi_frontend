ab4f79cf52c0fe72cf6b8bd1b8fe6946c6ca33ba
류종혁 <rjhsep11@gamil.com>
2025-10-14 17:14:16 +0900

    App.js 다시수정

--- project/src/App.js
+++ project/src/App.js
@@ -1,39 +1,17 @@
 import './App.css'
 import { Routes, Route} from 'react-router-dom'
 import Main from './component/Main'
-<<<<<<< HEAD
 import LoginMain from './component/LoginMain'
 import Layout from './component/Layout'
 import MyPage from './component/MyPage'
-=======
-import Myfooter from './component/Myfooter'
-import Navigation from './component/Navigation'
-import LoginMain from './component/LoginMain'
-import Register from './component/Register'
-
-function Layout() {
-  return (
-    <>
-      <Navigation />
-      <Outlet /> 
-      <Myfooter />
-    </>
-  )
-}
->>>>>>> project/main
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Main />} />
-<<<<<<< HEAD
         <Route path='login' element={<LoginMain />} />
         <Route path='mypage' element={<MyPage />} />
-=======
-        <Route path="login" element={<LoginMain />} />
-        <Route path="register" element={<Register/>} />
->>>>>>> project/main
       </Route>
     </Routes>
   )
