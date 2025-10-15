4f370adcaffd3ed2d2c5f3e64cf09e073e879e44
류종혁 <rjhsep11@gamil.com>
2025-10-15 15:07:24 +0900

    mypage 수정

--- project/src/App.js
+++ project/src/App.js
@@ -1,20 +1,20 @@
 import './App.css'
 import { Routes, Route,Outlet} from 'react-router-dom'
 import Main from './component/Main'
-import LoginMain from './component/LoginMain'
-import Layout from './component/Layout'
-import MyPage from './component/MyPage'
 import MyPage from './component/MyPage'
 import Myfooter from './component/Myfooter'
 import Navigation from './component/Navigation'
 import LoginMain from './component/LoginMain'
 import Register from './component/Register'
+import { Container } from 'react-bootstrap'
 
 function Layout() {
   return (
     <>
       <Navigation />
-      <Outlet /> 
+      <Container style={{ maxWidth: '1024px' }}> {/* 본문만 제한 */}
+        <Outlet />
+      </Container> 
       <Myfooter />
     </>
   )
